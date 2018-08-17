d5675c1b4bcd27ce3321e9d6eee04d7f7889ab4c
plusls <plusls@qq.com>
2018-08-18 01:25:49 +0800

    添加测试样例

--- get_pwn_data.py
+++ get_pwn_data.py
@@ -4,8 +4,8 @@ def get_pwn_data(token):
     	return ('note', "cnss{it_is_note_01}")
     elif token == 'note_02':
         return ('note', 'cnss{it_is_note_02}')
-
     if token == 'pwn1_01':
     	return ('pwn1', "cnss{it_is_pwn1}")
-
+    if token == 'sh':
+        return ('sh', 'cnss{test_hahah}')
     return('','')
