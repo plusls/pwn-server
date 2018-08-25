20820e21f5f1fbdb84cca640ef119fc3cc811d21
plusls <plusls@qq.com>
2018-08-26 01:09:09 +0800

    add log

--- mysocket.py
+++ mysocket.py
@@ -27,8 +27,8 @@ def set_keepalive_linux(sock, after_idle_sec=1, interval_sec=3, max_fails=5):
     then sends a keepalive ping once every 3 seconds (interval_sec),
     and closes the connection after 5 failed ping (max_fails), or 15 seconds
     """
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
-    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, after_idle_sec)
-    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, interval_sec)
-    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, max_fails)
+    print(sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1))
+    print(sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, after_idle_sec))
+    print(sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, interval_sec))
+    print(sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, max_fails))
 
