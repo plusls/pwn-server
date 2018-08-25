49f30fa0c4986f8b8d9fe926d1846b81e24b7032
plusls <plusls@qq.com>
2018-08-26 00:07:55 +0800

    设置keepalive

--- pwnserver.py
+++ pwnserver.py
@@ -46,8 +46,22 @@ def recvuntil(connect_socket, s):
         connect_socket.recv(1000)
 
 
+def set_keepalive_linux(sock, after_idle_sec=1, interval_sec=3, max_fails=5):
+    """Set TCP keepalive on an open socket.
+ 
+    It activates after 1 second (after_idle_sec) of idleness,
+    then sends a keepalive ping once every 3 seconds (interval_sec),
+    and closes the connection after 5 failed ping (max_fails), or 15 seconds
+    """
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, after_idle_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, interval_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, max_fails)
+
+
 def handle_connect(connect_socket):
     connect_time = str(time.time())
+    set_keepalive_linux(connect_socket)
     connect_socket.send(b'input your token:')
     try:
         token = recvuntil(connect_socket, b'\n')[:-1]
