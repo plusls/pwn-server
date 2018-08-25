d0c2d4a196a5028137b81811e0d60af098ea3720
plusls <plusls@qq.com>
2018-08-26 01:34:32 +0800

    debug

--- mysocket.py
+++ mysocket.py
@@ -3,9 +3,10 @@ def myrecv(s, buffersize, flag=0):
     while True:
         try:
             return s.recv(buffersize, flag)
-        except socket.timeout as e:
-            print('timeout {}'.format(e))
+        except socket.timeout:
             continue
+        except TimeoutError:
+            return b''
 
 
 def recvuntil(connect_socket, s, maxlen=200):
@@ -27,8 +28,8 @@ def set_keepalive_linux(sock, after_idle_sec=1, interval_sec=3, max_fails=5):
     then sends a keepalive ping once every 3 seconds (interval_sec),
     and closes the connection after 5 failed ping (max_fails), or 15 seconds
     """
-    print(sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1))
-    print(sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, after_idle_sec))
-    print(sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, interval_sec))
-    print(sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, max_fails))
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, after_idle_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, interval_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, max_fails)
 
